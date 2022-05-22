8687cb58c3e22e29f1808bea13432ad89296ae39
shahadat2772 <shahadat2772@gmailcom>
2022-05-22 20:43:18 +0600

    banner slightly modified

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     <section className="px-4">
       <div
         style={{
-          backgroundImage: `url('https://i.ibb.co/V27KmPT/BG-MAIN-SUPER.jpg')`,
+          backgroundImage: `url('https://i.ibb.co/DzWFrjh/banner-1.jpg')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -14,14 +14,20 @@ const Banner = () => {
         <div
           style={{
             position: "absolute",
-            top: "45%",
-            left: "20%",
+            // top: "25%",
+            // left: "15%",
           }}
-          className="bannerContent"
+          className="bannerContent left-[10%] md:left-[15%] top-[25%]"
         >
-          <h1 className="text-3xl text-white">
-            ENHANCE YOUR YOUR <br /> BUSINESS
+          <h1 className="text-5xl text-white">
+            LIFT YOUR YOUR <br /> BUSINESS{" "}
+            <span className="text-secondary">WITH US</span>
           </h1>
+          <p className="text-white text- mt-4 mb-5">
+            By improving your products quality. We believe in <br /> QUALITY,
+            RELIABILITY.
+          </p>
+          <button className="btn btn-secondary">Start NOw</button>
         </div>
       </div>
     </section>
