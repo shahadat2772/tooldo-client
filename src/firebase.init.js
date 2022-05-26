1881ea8fbdd42b55fbd8dc0c2ddbf700d5b96250
shahadat2772 <shahadat2772@gmailcom>
2022-05-26 17:12:48 +0600

    navber size issue fixed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,19 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  //   apiKey: process.env.REACT_APP_apiKey,
-  //   authDomain: process.env.REACT_APP_authDomain,
-  //   projectId: process.env.REACT_APP_projectId,
-  //   storageBucket: process.env.REACT_APP_storageBucket,
-  //   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  //   appId: process.env.REACT_APP_appId,
-
-  apiKey: "AIzaSyCJg0DL-pgc_YUD6r8FddPkBJbkO-d3ptU",
-  authDomain: "assignment-12-29c23.firebaseapp.com",
-  projectId: "assignment-12-29c23",
-  storageBucket: "assignment-12-29c23.appspot.com",
-  messagingSenderId: "122765750426",
-  appId: "1:122765750426:web:75ed4bfb2e9523f43a6f23",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
